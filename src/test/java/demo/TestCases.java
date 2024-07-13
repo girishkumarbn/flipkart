84ed7c55d420c1eadf6a70f4b6ad011b4a9b5f16
girishkumar-bn <girishkumar.bn@gmail.com>
2024-07-13 12:18:47 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -9,14 +10,19 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
-
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import java.time.Duration; 
+import java.util.Collections;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class TestCases {
     ChromeDriver driver;
@@ -25,6 +31,117 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+       @Test
+       public void testCase01() throws InterruptedException{
+       String url = "https://www.flipkart.com";
+       Wrappers.wrapperNavigate(driver, url);
+       
+       /* search for washing machine */
+       WebElement searchBar = driver.findElement(By.name("q"));
+       String searchItem = "Washing Machine";
+       searchBar.clear();
+       Wrappers.wrapperEnterText(searchBar, searchItem);
+       searchBar.sendKeys(Keys.ENTER);
+
+       WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		//wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='XQDdHH']")));
+       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='zg-M3Z _0H7xSG']")));
+
+        /* click of popularity link */
+       WebElement popularity = driver.findElement(By.xpath("//div[@class='zg-M3Z _0H7xSG']"));
+	   Wrappers.wrapperClick(popularity);
+	   System.out.println("Clicked on popularity");
+		
+		//wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='XQDdHH']")));
+       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Next']")));
+
+       List<WebElement> starRating = driver.findElements(By.xpath("//div[@class='XQDdHH']"));
+	   System.out.println("List Size"+ " "+starRating.size());
+       int countItems=0;
+	   for(WebElement stars:starRating){
+			String ratings = stars.getText();
+			//float starsRate = Integer.parseInt(ratings);
+			float starsRate = Float.parseFloat(ratings);
+			if(starsRate >= 4){
+				countItems++;
+			}
+		}
+		System.out.println("Total Items with Star Rating less than or equal to 4 -->"+" " +countItems);
+     }
+
+    @Test
+    public void testCase02() throws InterruptedException{
+    String url = "https://www.flipkart.com";
+    Wrappers.wrapperNavigate(driver, url);
+       
+    /* search for washing machine */
+    WebElement searchBar = driver.findElement(By.name("q"));
+    searchBar.clear();
+    String searchItem = "iPhone";
+    Wrappers.wrapperEnterText(searchBar, searchItem);
+    searchBar.sendKeys(Keys.ENTER);
+
+    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Next']")));
+
+    List<WebElement> title = driver.findElements(By.xpath("//div[@class='KzDlHZ']"));
+		for(WebElement titleText:title){
+			String actualTitle = titleText.getText();
+			WebElement percentOfOffer = driver.findElement(By.xpath("//div[@class='UkUFwK']//span"));
+				 String offer =  percentOfOffer.getText();
+				 int discount = Integer.parseInt(offer.replaceAll("[^0-9]", ""));
+				 if(discount >=17){
+					 System.out.println("Title "+" "+actualTitle);
+					 System.out.println("Discount" +" "+ discount);
+				 }
+			}
+	}
+
+    @Test
+    public void testCase03() throws InterruptedException {
+    ArrayList<Integer> reviewList = new ArrayList<>();
+    String url = "https://www.flipkart.com";
+    Wrappers.wrapperNavigate(driver, url);
+       
+    /* search for coffee mug */
+    WebElement searchBar = driver.findElement(By.name("q"));
+    searchBar.clear();
+    String searchItem = "Coffee Mug";
+    Wrappers.wrapperEnterText(searchBar, searchItem);
+    searchBar.sendKeys(Keys.ENTER);
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Next']")));
+
+    Wrappers.wrapperAdvancedScroll(driver);
+    Thread.sleep(3000);
+
+    WebElement fourStarRating = driver.findElement(By.xpath("//div[@class='XqNaEv']"));
+    Wrappers.wrapperAdvancedClick(fourStarRating);
+
+    Thread.sleep(3000);
+
+    List<WebElement> reviews = driver.findElements(By.xpath("//div//span[@class='Wphh3N']"));
+    for (WebElement review : reviews) {
+        String ratings = review.getText();
+        int ratingsValue = Integer.parseInt(ratings.replaceAll("[^0-9]", ""));
+        reviewList.add(ratingsValue);
+    }
+    
+    Collections.sort(reviewList, Collections.reverseOrder());
+    
+    for (int i = 0; i < 5 && i < reviewList.size(); i++) {
+        int reviewValue = reviewList.get(i);
+        String formattedValue = Wrappers.formatNumber(reviewValue);
+        System.out.println("Formatted Value" + formattedValue);
+        
+        // Note the use of single quotes around the formattedValue in the XPath
+        WebElement titleOfMug = driver.findElement(By.xpath("//span[contains(text(),'" + formattedValue + "')]/../../a[@class='wjcEIp']"));
+        System.out.println("Title of the Coffee Mug: " + titleOfMug.getAttribute("title"));
+        System.out.println("URL of the Coffee Mug: " + titleOfMug.getAttribute("href"));
+    }
+}
+
 
      
     /*
@@ -36,7 +153,7 @@ public class TestCases {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+        WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -5,8 +5,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.openqa.selenium.JavascriptExecutor;
 import java.util.stream.Collectors;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -14,9 +15,100 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+     public static boolean wrapperNavigate(ChromeDriver driver, String url){
+        try{
+            if(driver.getCurrentUrl().equals(url)){
+            return true;
+        }else{
+            driver.get(url);
+            return driver.getCurrentUrl() == url;
+        }
+     }catch(Exception e){
+        e.printStackTrace();
+        return false;
+         }
+    }
+
+    public static boolean wrapperClick(WebElement elememt){
+        try{
+            elememt.click();
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean wrapperEnterText(WebElement textBox, String inputText) throws InterruptedException {
+		try {
+			textBox.clear();
+			textBox.sendKeys(inputText);
+			return true;
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+    public static boolean wrapperAdvancedClick(WebElement button) throws InterruptedException{
+		try{
+			button.click();
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+    public static boolean wrapperAdvancedScrollWithOutElement(ChromeDriver driver, WebElement element) throws InterruptedException {
+		try {
+			 	JavascriptExecutor js = (JavascriptExecutor) driver;
+		        js.executeScript("arguments[0].scrollIntoView();", element);
+		        return true;
+		}catch(Exception e) {
+			return false;
+		}
+	}
+
+    public static boolean wrapperAdvancedScroll(ChromeDriver driver) throws InterruptedException{
+        try{
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+		    js.executeScript("window.scrollBy(0,230)");
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    // public static int formatNumber(int value){
+    //     NumberFormat myFormat = NumberFormat.getInstance(); 
+    //     myFormat.setGroupingUsed(true);  
+    //     String formatValue = myFormat.format(value);
+    //     return value;
+    // }
+
+        public static String formatNumber(int value){
+            NumberFormat myFormat = NumberFormat.getInstance(); 
+            myFormat.setGroupingUsed(true);  
+            String formatValue = myFormat.format(value);
+            //System.out.println("Formatted Value: " + formatValue);
+            return formatValue;
+        }
+
+    // public static int parseFormattedNumber(String formattedValue) throws ParseException {
+    //     NumberFormat myFormat = NumberFormat.getInstance();
+    //     myFormat.setGroupingUsed(true);
+    //     Number number = myFormat.parse(formattedValue);
+    //     return number.intValue();
+    // }
 }
